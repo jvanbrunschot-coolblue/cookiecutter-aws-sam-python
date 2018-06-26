10d31dd74e9e3118aeb2a558322a8dde985f2658
Jamie van Brunschot <j.vanbrunschot@coolblue.nl>
2018-06-26 16:34:21 +0200

    123

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,31 +6,15 @@
 
 from __future__ import print_function
 
-import os
-
 TERMINATOR = "\x1b[0m"
 INFO = "\x1b[1;33m [INFO]: "
 SUCCESS = "\x1b[1;32m [SUCCESS]: "
 HINT = "\x1b[3;33m"
 
 
-# def remove_optional_files():
-#     filenames = ["Makefile"]
-#     for file in filenames:
-#         if os.path.isfile(file):
-#             print(INFO + "Removing {} from project due to chosen options...".
-#                   format(file) + TERMINATOR)
-#             os.remove(file)
-#
-#     return True
-
-
 def main():
 
     project_name = '{{ cookiecutter.project_name }}'
-    # makefile_choice = '{{ cookiecutter.include_experimental_make }}'.lower()
-    # if makefile_choice == 'n':
-    #     remove_optional_files()
 
     print(SUCCESS +
           "Project initialized successfully! You can now jump to {} folder".
